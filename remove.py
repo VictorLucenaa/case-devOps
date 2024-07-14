0b5e7f6afbb6f8ac88a9e18f1598a44499abf964
Victor Lucena <victor.lucenaaa@gmail.com>
2024-07-14 11:14:18 -0300

    feat: add update_ports_configuration and change env properties functions

--- remove.py
+++ remove.py
@@ -1,6 +1,6 @@
 import os
 import yaml
-
+from datetime import datetime
 
 print('Inicializando o script')
 # Criar o arquivo logs.txt na mesma raiz do script
@@ -36,19 +36,38 @@ def update_kustomization_version(file_path, yaml_data):
     if modified:
         with open(file_path, 'w') as file:
             yaml.dump(yaml_data, file, default_flow_style=False, sort_keys=False)
-        with open('logs.txt', 'a') as log_file:
-            log_file.write(f"Modificações realizadas no arquivo: {file_path}\n")
-            log_file.write("- Atualizada versão na propriedade 'helmCharts' para '2.2.1'\n")
+    return modified
+
+def update_ports_configuration(yaml_data):
+    modified = False
+    if 'ports' in yaml_data:
+        yaml_data['ports'] = [
+            {
+                "name": "http",
+                "containerPort": "$PORT",
+                "protocol": "TCP"
+            }
+        ]
+        modified = True
     return modified
 
 # Função para atualizar o arquivo YAML com as modificações
 def update_yaml_file(file_path, yaml_data, modifications):
-    with open(file_path, 'w') as file:
-        yaml.dump(yaml_data, file, default_flow_style=False, sort_keys=False)
-    with open('logs.txt', 'a') as log_file:
-        log_file.write(f"Modificações realizadas no arquivo: {file_path}\n")
-        for modification in modifications:
-            log_file.write(f"- {modification}\n")
+    modified = False
+    if 'env' in yaml_data and yaml_data['env'] != 'ENV:dev':
+        yaml_data['env'] = 'ENV:dev'
+        modified = True
+        with open('logs.txt', 'a') as log_file:
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            log_file.write(f"\n[{timestamp}] - Modificações realizadas no arquivo: {file_path}\n")
+            log_file.write("- Atualizado valor da propriedade 'env' para 'ENV:dev'\n")
+    if modified:
+        with open(file_path, 'w') as file:
+            yaml.dump(yaml_data, file, default_flow_style=False, sort_keys=False)
+        with open('logs.txt', 'a') as log_file:
+            for modification in modifications:
+                log_file.write(f"- {modification}\n")
+    return modified
 
 # Função para percorrer o diretório atual e suas subpastas
 def process_directory(directory):
@@ -61,8 +80,9 @@ def process_directory(directory):
                     if update_kustomization_version(kustomization_path, yaml_data):
                         print(f"Propriedade 'version' atualizada no arquivo: {kustomization_path}")
                         with open('logs.txt', 'a') as log_file:
-                            log_file.write(f"Modificações realizadas no arquivo: {kustomization_path}\n")
-                            log_file.write("- Atualizado o valor de 'version' para '2.2.1'\n")
+                            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                            log_file.write(f"\n[{timestamp}] - Modificações realizadas no arquivo: {kustomization_path}\n")
+                            log_file.write("- Atualizado o valor de 'version' na configuração 'helmCharts' para '2.2.1'\n")
                 except yaml.YAMLError as e:
                     print(f"Erro ao processar o arquivo {kustomization_path}: {e}")
         for file in files:
@@ -73,11 +93,12 @@ def process_directory(directory):
                         yaml_data = yaml.safe_load(file)
                         modifications = []
                         if remove_redis_property(yaml_data):
-                            modifications.append("Remover propriedade 'redis' ou 'redisConfig'")
+                            modifications.append("Removida a propriedade 'redis' ou 'redisConfig'")
                         if 'ingress' in yaml_data and remove_ingress_ssl_redirect(yaml_data['ingress']):
-                            modifications.append("Remover anotação 'nginx.ingress.kubernetes.io/ssl-redirect' da propriedade 'ingress'")
-                        if modifications:
-                            update_yaml_file(file_path, yaml_data, modifications)
+                            modifications.append("Removida a anotação 'nginx.ingress.kubernetes.io/ssl-redirect' da propriedade 'ingress'")
+                        if update_ports_configuration(yaml_data):
+                            modifications.append("Atualizada a configuração 'ports'")    
+                        if update_yaml_file(file_path, yaml_data, modifications):
                             print(f"Modificações realizadas no arquivo: {file_path}")
                     except yaml.YAMLError as e:
                         print(f"Erro ao processar o arquivo {file_path}: {e}")
